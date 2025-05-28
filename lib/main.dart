0ba228ef247438950d0145c0b5f45b62da716785
seyedmohammadali2000 <sma.altaha.2000@gmail.com>
2025-05-28 19:17:53 +0330

    add more code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(Application());
@@ -13,82 +14,87 @@ class Application extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'vazir'),
       home: Scaffold(
-        appBar: getAppBar(),
+        appBar: _getAppBar(),
         body: SafeArea(
-          child: getMainBody(),
+          child: _getMainBody(),
         ),
       ),
     );
   }
-}
 
-PreferredSizeWidget getAppBar() {
-  return AppBar(
-    elevation: 0,
-    backgroundColor: Colors.amberAccent,
-    title: Text('سیدمحمدعلی حسین قاسم'),
-  );
-}
+  PreferredSizeWidget _getAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.amberAccent,
+      title: Text('سیدمحمدعلی حسین قاسم'),
+    );
+  }
 
-Widget getMainBody() {
-  return SingleChildScrollView(
-    child: Column(
+  Widget _getMainBody() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _getheader(),
+        ],
+      ),
+    );
+  }
+
+  Widget _getheader() {
+    return Column(
       children: [
-        Text('1mohammadali'),
-        Text('2mohammadali'),
-        Text('3mohammadali'),
-        Text('4mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('mohammadali'),
-        Text('111mohammadali'),
+        SizedBox(height: 20),
+        CircleAvatar(
+          backgroundImage: AssetImage('images/mohammadali.png'),
+          radius: 100,
+        ),
+        SizedBox(height: 15),
+        Text(
+          'أنی محمد علي، أبرمج موبايل و ويب',
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w900),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 15),
+        Text(
+          'كلش أحب البرمجة وحلّ المسائل، ودوم أحاول أتعلم وأطوّر نفسي بهالمجال',
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 15),
+        _getRowicon()
       ],
-    ),
-  );
+    );
+  }
+
+  Widget _getRowicon() {
+    return Wrap(
+      spacing: 30.00,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.linkedin),
+          color: Colors.blueGrey,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.instagram),
+          color: Colors.blueGrey,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.whatsapp),
+          color: Colors.blueGrey,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.telegram),
+          color: Colors.blueGrey,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.github),
+          color: Colors.blueGrey,
+        )
+      ],
+    );
+  }
 }
